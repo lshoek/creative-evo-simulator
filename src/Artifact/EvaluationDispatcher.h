8ac474cb12c705e90ad76d47ae2af39b644dc493
lshoek <lesleymartina@hotmail.com>
2020-08-12 19:25:10 +0200

    moved aestheticevaluator and added quadtree submodule

---
+++ src/Artifact/EvaluationDispatcher.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "ofThread.h"
+#include "ofThreadChannel.h"
+#include "Artifact/EvaluationType.h"
+#include "Artifact/SimpleEvaluators.h"
+#include "Artifact/AestheticEvaluator.h"
+#include "Artifact/OrderlyCoverageEvaluator.h"
+
+class EvaluationDispatcher : public ofThread 
+{
+public:
+    ofEvent<const std::vector<double>&> onFitnessResponseReady;
+
+    EvaluationDispatcher();
+    ~EvaluationDispatcher();
+
+    void setup(EvaluationType type, uint32_t width, uint32_t height);
+    void queue(cv::Mat image, int generation = 0, int id = 0, bool report = true);
+    void queueResponse();
+
+private:
+    void update(ofEventArgs& a);
+    virtual void threadedFunction();
+
+    std::unique_ptr<EvaluatorBase> _evaluator;
+
+    struct ArtifactEntry
+    {
+        ArtifactEntry() {}
+        ArtifactEntry(cv::Mat im) : image(im) {}
+
+        cv::Mat image;
+        double fitness = -1.0;
+        int generation = 0;
+        int id = 0;
+        int report = 1;
+        int response = 0;
+    };
+    ofThreadChannel<ArtifactEntry> _evalQueue;
+    ofThreadChannel<ArtifactEntry> _updateQueue;
+
+    std::vector<double> _fitnessQueue;
+    bool bSetup = false;
+};
