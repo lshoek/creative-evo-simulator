24067185b7d00fc2133b485c30ff03c48d6a797e
lshoek <lesleyvanhoek@hotmail.com>
2022-02-03 00:01:04 +0100

    cleanup

--- src/_unused/PaintFitnessFunc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SimFitnessFunc.h"
-
-class PaintFitnessFunc : public SimFitnessFunc
-{
-public:
-	virtual isim_ticket queueEval(GenomeBase& genome, bool bMultiEval) override;
-	virtual double awaitEval(isim_ticket id) override;
-};
